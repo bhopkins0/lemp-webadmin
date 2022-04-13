aeff49873a98333c5573d2d750fb79e99a59c187
Brent Hopkins <contact@brenthopkins.me>
2022-04-13 17:29:20 -0500

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,2 +1,2 @@
-apt update && apt -y upgrade
-apt -y install nginx mysql-server php7.4-fpm php-mysql php-common
+apt update && apt upgrade
+apt install nginx mysql-server php7.4-fpm php-mysql php-common
