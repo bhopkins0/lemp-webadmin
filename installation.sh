76ebbf0ac6618eda6e218211af68c4308f89ed93
Brent Hopkins <contact@brenthopkins.me>
2022-04-13 18:21:36 -0500

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -1,2 +1,16 @@
+## General Installation
 apt update && apt upgrade
-apt install nginx mysql-server php7.4-fpm php-mysql php-common
+apt install wget nginx mysql-server php7.4-fpm php-mysql php-common
+clear
+
+### MySQL Setup
+PASSWD=$(sed "s/[^a-zA-Z0-9]//g" <<< $(cat /dev/urandom | tr -dc 'a-zA-Z0-9!@#$%*()-+' | fold -w 15 | head -n 1))"!"
+mysql_secure_installation
+wget -O mysqlsetup https://raw.githubusercontent.com/bhopkins0/lemp-webadmin/main/mysqlsetup
+clear
+mysql --execute "CREATE DATABASE nginxmanager;"
+mysql --execute "CREATE USER 'nginxmanager'@'localhost' IDENTIFIED BY '$PASSWD';"
+mysql --execute "GRANT ALL PRIVILEGES ON nginxmanager.* TO 'nginxmanager'@'localhost';"
+mysql --execute "FLUSH PRIVILEGES;";
+mysql --execute "USE nginxmanager; source mysqlsetup;";
+echo "Your MySQL username is: nginxmanager \nYour MySQL password is: $PASSWD";
