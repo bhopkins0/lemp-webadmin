d2690317e66346db7b0dadbbf0aa66f3d069ed39
Brent Hopkins <contact@brenthopkins.me>
2022-04-14 11:45:43 -0500

    Update installation.php

--- webadmin/installation.php
+++ webadmin/installation.php
@@ -1,6 +1,6 @@
 <?php
 session_start();
-include '../config.php';
+include '/var/www/config.php';
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
         if (!isset($_POST["admin_user"])) {
                 $_SESSION["err"] = 8;
